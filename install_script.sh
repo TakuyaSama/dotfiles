4c74fc7b2414a2ee15a48f0a7c8d1fff90adf84e
Takuya <takuya@disroot.org>
2020-07-22 14:32:29 +0200

    Added the instructions to link Zsh, Neovim & tmux configuration files to its respectives default paths

--- install_script.sh
+++ install_script.sh
@@ -1,2 +1,9 @@
 #/bin/sh
+# Link Zsh configuration file to its default path
+ln -s ./Zsh/.zshrc ~/.zshrc
 
+# Link Neovim configuration file to its default path
+ln -s ./Neovim/init.vim ~/.config/nvim/init.vim
+
+# Link tmux configuration file to its default path
+ln -s ./tmux/.tmux.conf ~/.tmux.conf
