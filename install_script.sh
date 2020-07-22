3a189a70eef9267b6764c3bd62f0a12eef5670c1
Takuya <takuya@disroot.org>
2020-07-22 14:38:28 +0200

    Fixed a typo on the header installation script line

--- install_script.sh
+++ install_script.sh
@@ -1,9 +1,9 @@
-#/bin/sh
+#!/bin/sh
 # Link Zsh configuration file to its default path
-ln -s ./Zsh/.zshrc ~/.zshrc
+ln ./Zsh/.zshrc ~/.zshrc
 
 # Link Neovim configuration file to its default path
-ln -s ./Neovim/init.vim ~/.config/nvim/init.vim
+ln ./Neovim/init.vim ~/.config/nvim/init.vim
 
 # Link tmux configuration file to its default path
-ln -s ./tmux/.tmux.conf ~/.tmux.conf
+ln ./tmux/.tmux.conf ~/.tmux.conf
