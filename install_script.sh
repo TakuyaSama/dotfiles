78b2e838fb1c990c652ecf20dadb28e7a6aa20a3
Takuya <takuya@disroot.org>
2020-07-23 11:53:57 +0200

    Updated & added Alacritty link at the installation script, also added an echo per each instruction realized

--- install_script.sh
+++ install_script.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # Link Zsh configuration file to its default path
 ln ./Zsh/.zshrc ~/.zshrc
+echo "Zsh configuration file linked"
 
 # Link Neovim configuration file to its default path
 ln ./Neovim/init.vim ~/.config/nvim/init.vim
+echo "Neovim configuration file linked"
 
 # Link tmux configuration file to its default path
 ln ./tmux/.tmux.conf ~/.tmux.conf
+echo "tmux configuration file linked"
+
+# Link Alacritty configuration file to its default path
+ln ./Alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+echo "Alacritty configuration file linked"
