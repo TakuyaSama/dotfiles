79b6389371ad3f53c51fb558173bb0648d2488d5
Takuya <takuya@disroot.org>
2020-08-04 18:50:04 +0200

    Added instructions for Zsh plugin manager 'Antigen' & for Zsh theme, 'powerlevel10k'

--- install_script.sh
+++ install_script.sh
@@ -3,6 +3,14 @@
 ln ./Zsh/.zshrc ~/.zshrc
 echo "Zsh configuration file linked"
 
+# Link Zsh Plugin Manager Antigen
+ln ./Zsh/.antigen.zsh ~/.antigen.zsh
+echo "Zsh plugin manager antigen file linked"
+
+# Link Zsh Theme Powerlevel10K
+ln ./Zsh/.p10k.zsh ~/.p10k.zsh
+echo "Zsh theme powerlevel10K file linked"
+
 # Link Neovim configuration file to its default path
 ln ./Neovim/init.vim ~/.config/nvim/init.vim
 echo "Neovim configuration file linked"
