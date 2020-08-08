efed7c0be3aff9766b5c449bcc2abb6f658d88b5
Takuya <takuya@disroot.org>
2020-08-08 19:26:15 +0200

    Added the force flag to ln command, to prevent from errors

--- install_script.sh
+++ install_script.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 # Link Zsh configuration file to its default path
-ln ./Zsh/.zshrc ~/.zshrc
+ln -f ./Zsh/.zshrc ~/.zshrc
 echo "Zsh configuration file linked"
 
 # Link Zsh Plugin Manager Antigen
-ln ./Zsh/.antigen.zsh ~/.antigen.zsh
+ln -f ./Zsh/.antigen.zsh ~/.antigen.zsh
 echo "Zsh plugin manager antigen file linked"
 
 # Link Zsh Theme Powerlevel10K
-ln ./Zsh/.p10k.zsh ~/.p10k.zsh
+ln -f ./Zsh/.p10k.zsh ~/.p10k.zsh
 echo "Zsh theme powerlevel10K file linked"
 
 # Link Neovim configuration file to its default path
-ln ./Neovim/init.vim ~/.config/nvim/init.vim
+ln -f ./Neovim/init.vim ~/.config/nvim/init.vim
 echo "Neovim configuration file linked"
 
 # Link tmux configuration file to its default path
-ln ./tmux/.tmux.conf ~/.tmux.conf
+ln -f ./tmux/.tmux.conf ~/.tmux.conf
 echo "tmux configuration file linked"
 
 # Link Alacritty configuration file to its default path
-ln ./Alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -f ./Alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 echo "Alacritty configuration file linked"
 
 # Link lf configuration file to its default path
-ln ./lf/lfrc ~/.config/lf/lfrc
+ln -f ./lf/lfrc ~/.config/lf/lfrc
 echo "lf configuration file linked"
